dd2122beeb556795659cc2063e6126503a8a4319
Alexandre Fernandes <alexandrefernandes.132132@gmail.com>
2023-10-01 15:59:55 -0300

    laboratorio 01 -primeiro recurso

---
+++ Social_Books_Api-main/src/main/java/br/fepi/socialbooks/domain/Livro.java
@@ -0,0 +1,77 @@
+package br.fepi.socialbooks.domain;
+
+import java.util.Date;
+import java.util.List;
+
+public class Livro {
+	private Long id;
+	
+	private String nome;
+	
+	private Date publicacao;
+	
+	private String editora;	
+
+	private String resumo;
+	
+	private String autor;
+	
+	private List<Comentario> comentarios ;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getPublicacao() {
+		return publicacao;
+	}
+
+	public void setPublicacao(Date publicacao) {
+		this.publicacao = publicacao;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
+}
