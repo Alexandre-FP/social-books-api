647943250b5aca7e6f3c797b33d488762f70cad4
Alexandre Fernandes <101991066+Alexandre-FP@users.noreply.github.com>
2023-10-24 19:24:33 -0300

    Add files via upload

--- Social_Books_Api-main/api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
+++ Social_Books_Api-main/api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
@@ -1,15 +1,29 @@
 package br.fepi.socialbooks.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.fepi.socialbooks.domain.Livro;
+
 @RestController
 public class LivrosResources {
 	
-	@RequestMapping(value = "/livros",
-			method = RequestMethod.GET)
-	public String listar() {
-		return "O pequeno príncipe; Os segredos da mente milionária";
+	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	
+	public List<Livro>  listar() {
+		Livro livro1 = new Livro("a arte da guerra");
+		Livro livro2 = new Livro("O pequeno príncipe");
+		Livro livro3 = new Livro("1984");
+		
+		Livro[] livros = {
+				livro1, livro2,livro3
+		};
+		
+		return Arrays.asList(livros);
 	}
+	
 }
