ff8cd8fc66169122a88938803f362660caabf063
Alexandre-FP <alexandrefernandes.132132@gmail.com>
2023-11-14 19:50:31 -0300

    Aula 14-11

---
+++ api_social_books/src/main/java/br/fepi/socialbooks/domain/Comentario.java
@@ -0,0 +1,57 @@
+package br.fepi.socialbooks.domain;
+
+import java.util.Date;
+
+public class Comentario {
+	
+	private Long id;
+	
+	private String texto;
+	
+	private String usuario;
+	
+	private Date data;
+	
+	private Livro livro;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}	
+	
+}
---
+++ api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
@@ -0,0 +1,64 @@
+package br.fepi.socialbooks.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.fepi.socialbooks.domain.Livro;
+import br.fepi.socialbooks.repository.LivrosRepository;
+
+@RestController
+@RequestMapping(value = "/livros")
+public class LivrosResources {
+	
+	@Autowired
+	private LivrosRepository livrosRepository;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Livro>  listar() {
+		Livro livro1 = new Livro("a arte da guerra");
+		Livro livro2 = new Livro("O pequeno príncipe");
+		Livro livro3 = new Livro("1984");
+		
+		Livro[] livros = {
+				livro1, livro2,livro3
+		};
+		
+		return Arrays.asList(livros);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		
+		livrosRepository.save(livro);
+	}
+	
+	//@RequestMapping(method = RequestMethod.GET)
+	//public List<Livro> listar(@RequestBody Livro livro) {
+	//	return livrosRepository.findAll();
+	//}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)	
+	public Livro buscaId(@PathVariable("id") long id) {
+		return livrosRepository.findById(id).orElse(null);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)	
+	public void deletar (@PathVariable("id") Long id) {
+		livrosRepository.deleteById(id);
+	}
+
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public void atualizar (@RequestBody Livro livro,@PathVariable("id") Long id) {
+		livro.setId(id);
+		livrosRepository.save(livro);
+	}
+	
+}
