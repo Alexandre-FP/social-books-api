745302ece9db883e3440ce1ccdd3fb3b90d946d0
Alexandre Fernandes <101991066+Alexandre-FP@users.noreply.github.com>
2023-11-21 19:37:01 -0300

    Update LivrosResources.java

--- api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
+++ api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
@@ -1,14 +1,15 @@
 package br.fepi.socialbooks.resources;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController ;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.fepi.socialbooks.domain.Livro;
 import br.fepi.socialbooks.repository.LivrosRepository;
@@ -20,18 +21,18 @@ public class LivrosResources {
 	@Autowired
 	private LivrosRepository livrosRepository;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Livro>  listar() {
-		Livro livro1 = new Livro("a arte da guerra");
-		Livro livro2 = new Livro("O pequeno príncipe");
-		Livro livro3 = new Livro("1984");
+	//@RequestMapping(method = RequestMethod.GET)
+	//public List<Livro>  listar() {
+		//Livro livro1 = new Livro("a arte da guerra");
+		//Livro livro2 = new Livro("O pequeno príncipe");
+		//Livro livro3 = new Livro("1984");
 		
-		Livro[] livros = {
-			livro1, livro2,livro3
-		};
+		//Livro[] livros = {
+			//	livro1, livro2,livro3
+	//	};
 		
-		return Arrays.asList(livros);
-	}
+	//	return Arrays.asList(livros);
+	//}
 
 	@RequestMapping(method = RequestMethod.POST)
 	public void salvar(@RequestBody Livro livro) {
@@ -39,11 +40,12 @@ public class LivrosResources {
 		livrosRepository.save(livro);
 	}
 	
-	//@RequestMapping(method = RequestMethod.GET)
-	//public List<Livro> listar(@RequestBody Livro livro) {
-	//	return livrosRepository.findAll();
-	//}
-
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Livro> listar(@RequestBody Livro livro) {
+		return livrosRepository.findAll();
+	}
+	
+	@CrossOrigin
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)	
 	public Livro buscaId(@PathVariable("id") long id) {
 		return livrosRepository.findById(id).orElse(null);
