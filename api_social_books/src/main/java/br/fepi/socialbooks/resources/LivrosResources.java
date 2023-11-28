f0e1cb6440449035d0b72d3147e8eeea1a13a845
Alexandre-FP <alexandrefernandes.132132@gmail.com>
2023-11-28 19:55:59 -0300

    aula 05

--- api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
+++ api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,39 +22,37 @@ import br.fepi.socialbooks.repository.LivrosRepository;
 
 @RestController
 @RequestMapping(value = "/livros")
-public class LivrosResources {
+public class LivrosResources { 
 	
 	@Autowired
-	private LivrosRepository livrosRepository; 
+	private LivrosRepository livrosRepository;
 
 	@Autowired
 	private ComentarioRepository comentariosRepository; 
 
-	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {
-		
-		livrosRepository.save(livro);
-	}
-	
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Livro> listar(@RequestBody Livro livro) {
+	public List<Livro> listar() {
 		return livrosRepository.findAll();
 	}
-	
+
+	@PostMapping()
+	public Livro salvar(@RequestBody Livro livro){
+		return livrosRepository.save(livro);
+	}
+
 	@CrossOrigin
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)	
-	public Livro buscaId(@PathVariable("id") long id) { 
+	@GetMapping(value = "/{id}")
+	public Livro findById(@PathVariable (value = "id")Long id){
 		return livrosRepository.findById(id).orElse(null);
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)	
-	public void deletar (@PathVariable("id") Long id) {
+	@DeleteMapping(value = "/{id}")
+	public void deletar(@PathVariable(value = "id")Long id){
 		livrosRepository.deleteById(id);
 	}
 
-
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public void atualizar (@RequestBody Livro livro,@PathVariable("id") Long id) {
+	@PutMapping(value = "{id}")
+	public void atualizar(@RequestBody Livro livro, @PathVariable("id") Long id){
 		livro.setId(id);
 		livrosRepository.save(livro);
 	}
@@ -61,6 +63,6 @@ public class LivrosResources {
 		livro.setId(livroId);
 
 		comentario.setLivro(livro);
-		comentariosRepository.save(comentario); 
+		comentariosRepository.save(comentario);  
 	}
 }
